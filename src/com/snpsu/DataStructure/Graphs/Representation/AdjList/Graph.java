fa258995e857f41226c3920d9a05952116d2560a
Keerthana7122006 <keerthanakeeru712@gmail.com>
2026-09-03 16:40:55 +0530

    Completed trees and graphs

---
+++ src/com/snpsu/DataStructure/Graphs/Representation/AdjList/Graph.java
@@ -0,0 +1,47 @@
+package com.snpsu.DataStructure.Graphs.Representation.AdjList;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Graph {
+       HashMap<Integer, ArrayList<Integer>>adjList;
+
+       Graph(int vertices){
+           adjList=new HashMap<>();
+           for(int i=0;i<vertices;i++){
+               adjList.put(i,new ArrayList<>());
+           }
+
+       }
+
+       void printAdjList(){
+           for(int vertex:adjList.keySet()){
+               System.out.print(vertex+"->");
+               for(int neighbour:adjList.get(vertex) ){
+                   System.out.print(neighbour+" ");
+               }
+               System.out.println();
+           }
+       }
+       void addEdge(int u,int v) {
+           adjList.get(u).add(v);
+           adjList.get(v).add(u);
+       }
+}
+class Drivercode {
+    public static void main(String[] args) {
+        Graph g = new Graph(6);
+        g.addEdge(0,1);
+        g.addEdge(0,5);
+        g.addEdge(0,4);
+        g.addEdge(1,2);
+        g.addEdge(1,3);
+        g.addEdge(1,4);
+        g.addEdge(2,3);
+        g.addEdge(3,4);
+        g.addEdge(4,5);
+        g.printAdjList();
+
+
+    }
+}
--- src/com/snpsu/DataStructure/Trees/BST/BST.java
+++ src/com/snpsu/DataStructure/Trees/BST/BST.java
@@ -1,100 +1,116 @@
-//package com.snpsu.DataStructure.Trees.BST;
-//
-//class Node {
-//    int data;
-//    Node left;
-//    Node right;
-//
-//    Node(int data) {
-//        this.data = data;
-//        this.left = null;
-//        this.right = null;
-//    }
-//}
-//
-//public class BST {
-//
-//    Node root;
-//
-//    private Node insertRec(Node root, int key) {
-//        if (root == null) {
-//            return new Node(key);
-//        }
-//
-//        if (key < root.data) {
-//            root.left = insertRec(root.left, key);
-//        } else if (key > root.data) {
-//            root.right = insertRec(root.right, key);
-//        }
-//
-//        return root;
-//    }
-//
-//    void insert(int key) {
-//        root = insertRec(root, key);
-//    }
-//
-//    private boolean searchRec(Node root, int key) {
-//
-//        if (root == null) {
-//            return false;
-//        }
-//
-//        if (key == root.data) {
-//            return true;
-//        } else if (key < root.data) {
-//            return searchRec(root.left, key);
-//        } else {
-//            return searchRec(root.right, key);
-//        }
-//    }
-//
-//    boolean search(int key) {
-//        return searchRec(root, key);
-//    }
-//
-//    void DFS(Node root) {
-//        if (root == null) {
-//            return;
-//        }
-//
-//        System.out.print(root.data + " ");
-//
-//        DFS(root.left);
-//        DFS(root.right);
-//    }
-//
-//
-//    private Node removeRec(Node root,int key){
-//        if(root==null) {
-//            return null;
-//        }
-//        if(key<root.data) {
-//            root.left = removeRec(root.left, key);
-//        }else if(key>root.data){
-//            root.right=removeRec(root.right,key);
-//        }else{
-//
-//        }
-//
-//    }
-//    void remove(){
-//        root=removeRec(root,key);
-//
-//    }
-//}
-//
-//class Driver {
-//    public static void main(String[] args) {
-//
-//        BST bst = new BST();
-//
-//        bst.insert(100);
-//        bst.insert(50);
-//        bst.insert(180);
-//
-//        bst.search(70);
-//
-//        bst.DFS(bst.root);
-//    }
-//}
+package com.snpsu.DataStructure.Trees.BST;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class BST {
+    Node root;
+    BST(){
+        root = null;
+    }
+
+    void DFS(Node root){
+        if(root==null){
+            return;
+        }
+        DFS(root.left);
+        System.out.print(root.data + " ");
+        DFS(root.right);
+    }
+
+    private Node insertRec(Node root, int key){
+        if(root==null){
+            return new Node(key);
+        }
+        if(key < root.data){
+            root.left = insertRec(root.left, key);
+        }else if(key > root.data){
+            root.right = insertRec(root.right, key);
+        }
+        return root;
+    }
+    void insert(int key){
+        root = insertRec(root, key);
+    }
+
+    private boolean searchRec(Node root, int key){
+        if(root==null){
+            return false;
+        }
+        if(key == root.data){
+            return true;
+        }else if(key <root.data){
+            return searchRec(root.left, key);
+        }else{
+            return searchRec(root.right, key);
+        }
+
+    }
+    boolean search(int key){
+        return searchRec(root, key);
+    }
+
+    private Node removeRec(Node root, int key){
+        if(root==null){
+            return null;
+        }
+        if(key < root.data){
+            root.left = removeRec(root.left, key);
+        }else if(key > root.data){
+            root.right = removeRec(root.right, key);
+        }else{
+            // u found the node to be deleted!
+            //case 1 Node with 0 or 1 child
+            if(root.left==null) {
+                return root.right;
+            }else if(root.right==null){
+                return root.left;
+            }
+            // Case 2 node with 2 children
+            Node successor=findSuccessor(root.right);
+            root.data=successor.data;
+
+
+            root.right=removeRec(root.right,successor.data);
+        }
+        return root;
+    }
+
+    private Node findSuccessor(Node node){
+        while(node.left!=null){
+            node=node.left;
+        }return node;
+
+    }
+    void remove(int key){
+        root = removeRec(root, key);
+    }
+
+}
+
+class Driver{
+    public static void main(String[] args) {
+        BST bst = new BST();
+        bst.insert(100);
+        bst.insert(50);
+        bst.insert(150);
+        bst.insert(30);
+        bst.insert(60);
+        bst.insert(120);
+        bst.insert(180);
+        bst.remove(55);
+        bst.insert(110);
+        bst.insert(130);
+
+        bst.DFS(bst.root);
+    }
+}
